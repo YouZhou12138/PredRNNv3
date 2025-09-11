2079eb2cf6b0c9cd6d7063e61783c038352e98a6
YouZhou12138 <2728879990@qq.com>
2025-09-11 17:25:21 +0800

    Add files via upload

---
+++ task/trainer_selection.py
@@ -0,0 +1,8 @@
+
+from task.st_single import SpatioTemporal_single
+Trainer = {
+    "moving_mnist": SpatioTemporal_single,
+    "kth": SpatioTemporal_single,
+    "taxibj": SpatioTemporal_single,
+    "weather": SpatioTemporal_single
+}

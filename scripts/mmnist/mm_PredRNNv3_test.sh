da9297cb7aaacd9df546af682d70d75d9bb130f7
YouZhou12138 <2728879990@qq.com>
2025-09-11 19:17:29 +0800

    Update mm_PredRNNv3_test.sh

--- scripts/mmnist/mm_PredRNNv3_test.sh
+++ scripts/mmnist/mm_PredRNNv3_test.sh
@@ -1,8 +1,8 @@
 cd ..
 cd ..
 python test.py \
-    --setting  configs\kth\PredRNNv3\6M\seed=27.yaml \
-    --checkpoint experiments\kth\PredRNNv3\7M\checkpoints\epoch=42-PSNR=30.9075.ckpt\
-    --data_name  kth \
+    --setting  configs\moving_mnist\PredRNNv3\6M\seed=1234.yaml \
+    --checkpoint experiments\moving_mnist\PredRNNv3\6M\checkpoints\epoch=1529-MSE=12.4183.ckpt\
+    --data_name  moving_mnist \
     --data_dir D:\dataset \
-    --pred_dir experiments\kth\PredRNNv3\7M\20
+    --pred_dir experiments/moving_mnist/PredRNNv3/6M/predictions
